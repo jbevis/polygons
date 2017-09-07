4c2ddd16897d9a9d3d083954d4d617f982d07f64
Jack Bevis <jack.bevis@gmail.com>
2017-09-07 15:19:01 -0600

    file structure and db setup

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/polygon_map',
+    migrations: {
+      directory: './db/migrations/'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+
+};
