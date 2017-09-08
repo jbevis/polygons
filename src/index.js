4b352c50be011de7b5bb0b4ecab0e5e0ec323b05
Jack Bevis <jack.bevis@gmail.com>
2017-09-08 12:11:44 -0600

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import Provider from 'react-redux';
+import AppContainer from './containers/AppContainerCon';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import RootReducer from './reducers/RootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(RootReducer, {}, devTools(applyMiddleware(thunk)));
+
+
+
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
 registerServiceWorker();
