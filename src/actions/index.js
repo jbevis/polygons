120f37be51a348dd1477b0b972a4434864da6aab
Jack Bevis <jack.bevis@gmail.com>
2017-09-08 12:08:39 -0600

    hooked up redux to AppContainer and wrote actions

---
+++ src/actions/index.js
@@ -0,0 +1,52 @@
+require ('isomorphic-fetch');
+
+const saveAllLocations = (locations) => {
+  return {
+    type: 'SAVE_LOCATIONS',
+    data: locations
+  };
+};
+
+const saveCoordinates = (coordinates) => {
+  return {
+    type: 'SAVE_COORDINATES',
+    data: locations
+  };
+};
+
+const setCurrentCoordinates = (coordinates) => {
+  return {
+    type: 'SET_CURRENT_COORDINATES',
+    data: coordinates
+  };
+};
+
+const fetchLocations = () => {
+  return (dispatch) => {
+    return fetch('/locations', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+    .then(locations => locations.json())
+    .then(json => dispatch(saveAllLocations(json));
+  };
+};
+
+const saveNewLocation = (location) => {
+  return (dispatch) => {
+    return fetch('/locations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(location)
+    })
+    .then(response => response.json())
+    .then(() => dispatch(fetchLocations()));
+  };
+};
+
+export { fetchLocations, saveNewLocation, saveCoordinates, setCurrentCoordinates };
